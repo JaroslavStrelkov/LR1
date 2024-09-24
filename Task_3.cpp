f446c842b7565470c59a17bef3ee4bf971978b08
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-09-24 23:34:26 +0300

    gtest

--- Task_3.cpp
+++ Task_3.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-int main() {
-double start, end, step;
-std::cout << "Введите начальное значение x: ";
-std::cin >> start; // Задаем исходное значение переменной
-std::cout << "Введите конечное значение x: ";
-std::cin >> end; // Задаем конечно значение переменной
-std::cout << "Введите шаг: ";
-std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
+#include <gtest/gtest.h>
+#include <cmath>
+double func(double start, double end, double step) {
+double sum = 0;
+// double touble, start, end, step;
+// std::cout << "Введите начальное значение x: ";
+// std::cin >> start; // Задаем исходное значение переменной
+// std::cout << "Введите конечное значение x: ";
+// std::cin >> end; // Задаем конечно значение переменной
+// std::cout << "Введите шаг: ";
+// std::cin >> step; // Задаем шаг, с которым с которым будет изменяться aргумент
 for (double x = start; x <= end; x += step) {
 double y = sin(x); // функция y = sin(x)
-std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
+sum += y;
+
+// std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
 }
-return 0;
+return sum;
+}
+
+TEST(func, first) {
+ASSERT_EQ(0, func(0,0,1));
+}
+int main(int argc, char** argv) 
+{
+
+    ::testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
 }
